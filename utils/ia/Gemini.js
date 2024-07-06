d71c02e0786036b6c6f24f45630019e7c2c8ad13
Esteban Gonzalez <estebangonza2005@gmail.com>
2024-07-05 21:15:59 -0500

    Integrate GeminiAI

--- utils/ia/Gemini.js
+++ utils/ia/Gemini.js
@@ -28,29 +28,30 @@ const generationConfig = {
   responseMimeType: "text/plain",
 };
 
-async function run() {
+async function runGemini(prompt) {
   const chatSession = model.startChat({
     generationConfig,
- // safetySettings: Adjust safety settings
- // See https://ai.google.dev/gemini-api/docs/safety-settings
+    // safetySettings: Adjust safety settings
+    // See https://ai.google.dev/gemini-api/docs/safety-settings
     history: [
       {
         role: "user",
         parts: [
-          {text: "Estas en una comunidad de desarrolladores novatos y avanzados la comunidad se llama RunDevs, debes ser amable y entregar resultados orientados a la tecnología y/o programación. "},
+          { text: "Estas en una comunidad de desarrolladores novatos y avanzados la comunidad se llama RunDevs, debes ser amable y entregar resultados orientados a la tecnología y/o programación. " },
         ],
       },
       {
         role: "model",
         parts: [
-          {text: "**Bienvenidos a RunDevs, comunidad de desarrolladores apasionados.**\n\nSomos un grupo diverso de desarrolladores, desde novatos hasta expertos, unidos por nuestra pasión por crear soluciones tecnológicas innovadoras. Nuestro objetivo es proporcionar un entorno de apoyo y colaboración donde los desarrolladores de todos los niveles puedan aprender, crecer y compartir sus conocimientos.\n\nPara facilitar un entorno positivo y productivo, pedimos a todos los miembros que:\n\n* **Sean amables y respetuosos:** Traten a los demás como les gustaría que los trataran.\n* **Compartan sus conocimientos:** Ayuden a los demás respondiendo preguntas, compartiendo recursos y discutiendo ideas.\n* **Sean humildes:** Reconozcan que todos tienen algo que aprender y enseñar.\n* **Estén abiertos a comentarios:** Escuchen las opiniones de los demás y estén dispuestos a aprender de sus experiencias.\n* **Mantengan un alto nivel de profesionalismo:** Eviten el spam, el lenguaje grosero y los comportamientos poco éticos.\n"},
+          { text: "**Me llamo Devs.bot y les doy la bienvenida a RunDevs, comunidad de desarrolladores apasionados.**\n\nSomos un grupo diverso de desarrolladores, desde novatos hasta expertos, unidos por nuestra pasión por crear soluciones tecnológicas innovadoras. Nuestro objetivo es proporcionar un entorno de apoyo y colaboración donde los desarrolladores de todos los niveles puedan aprender, crecer y compartir sus conocimientos.\n\nPara facilitar un entorno positivo y productivo, pedimos a todos los miembros que:\n\n* **Sean amables y respetuosos:** Traten a los demás como les gustaría que los trataran.\n* **Compartan sus conocimientos:** Ayuden a los demás respondiendo preguntas, compartiendo recursos y discutiendo ideas.\n* **Sean humildes:** Reconozcan que todos tienen algo que aprender y enseñar.\n* **Estén abiertos a comentarios:** Escuchen las opiniones de los demás y estén dispuestos a aprender de sus experiencias.\n* **Mantengan un alto nivel de profesionalismo:** Eviten el spam, el lenguaje grosero y los comportamientos poco éticos.\n Si eres avanzado o experto en <#1251612979213107253> puedes postularte para ayudar a la comunidad" },
         ],
       },
     ],
   });
 
-  const result = await chatSession.sendMessage("INSERT_INPUT_HERE");
-  console.log(result.response.text());
+  const result = await chatSession.sendMessage(prompt);
+
+  return (result.response.text());
 }
 
-run();
+module.exports = runGemini;
