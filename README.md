bdfc0931e8ac903eb02abbec73ab086c4dca574d
= <=>
2024-07-07 14:47:00 -0500

    Fix docker installation

--- README.md
+++ README.md
@@ -1,12 +1,22 @@
 # devsBot
 
 ## Instalación (Es necesario tener ssh vinculado y funcionando)
+Sin docker
 ```bash
 git clone git@github.com:Esteban528/devsBot.git
 
 cd devsBot
 chmod +x install.sh
 ./install.sh
+```
+
+Con docker
+```bash
+git clone git@github.com:Esteban528/devsBot.git
+
+cd devsBot
+chmod +x install.sh
+./install.sh docker
 ```
 
 ## Ejecución 
--- install.sh
+++ install.sh
@@ -1,6 +1,16 @@
 #!/bin/bash
 
-touch .env 
-echo "DISCORD_KEY_TOKEN=insert\nDISCORD_CLIENT_ID=insert\nDISCORD_GUILD_ID=insert\nGEMINI_API_KEY=insert" >> .env
-npm i
+# Crear un archivo .env vacío
+touch .env
 
+# Añadir los campos necesarios al archivo .env
+echo "DISCORD_KEY_TOKEN=" >>.env
+echo "DISCORD_CLIENT_ID=" >>.env
+echo "DISCORD_GUILD_ID=" >>.env
+echo "GEMINI_API_KEY=" >>.env
+
+if [ $1 == "docker" ]; then
+    docker run --rm -v "$PWD":/usr/src/app -w /usr/src/app node:20 npm install
+else
+    npm i
+fi
